63b02173eecbd75b284143be6b526f0724a9a703
j-ullrich <104524717+j-ullrich@users.noreply.github.com>
2024-09-05 17:33:25 +0200

    csv update

--- src/iDrink/iDrinkLog.py
+++ src/iDrink/iDrinkLog.py
@@ -17,7 +17,7 @@ def get_new_row(trial, columns):
     :param columns:
     :return:
     """
-    new_row = pd.Series()
+    new_row = pd.Series(dtype='object')
     for column in columns:
         try:
             new_row[column] = getattr(trial, column)
@@ -34,24 +34,27 @@ def update_trial_csv(args, trial_list,  csv_path, columns_to_add = None):
     :return:
     """
 
-    df_trials = pd.read_csv(csv_path, sep=';')
+    df_trials_old = pd.read_csv(csv_path, sep=';')
+    df_trials = pd.DataFrame(columns=df_trials_old.columns)
 
     if args.verbose >= 1:
         progress = tqdm(total=len(trial_list), desc="Updating CSV", unit="Trial")
     for trial in trial_list:
-        for column in df_trials.columns:
-            try:
-                df_trials.loc[df_trials.identifier == trial.identifier, column] = getattr(trial, column)
-            except:
-                df_trials.loc[df_trials.identifier == trial.identifier, column] = ', '.join(getattr(trial, column))
 
+        df_trials = pd.concat([df_trials, get_new_row(trial, df_trials.columns).to_frame().T], axis=0, ignore_index=True)
 
         if columns_to_add is not None:
+            df_trials = pd.concat([df_trials, get_new_row(trial, columns_to_add).to_frame().T], axis=0,
+                                  ignore_index=True)
+
+            """
+            Old Version -- Keep until new version is tested
             for column in columns_to_add:
                 try:
                     df_trials.loc[df_trials.identifier == trial.identifier, column] = getattr(trial, column)
                 except:
                     df_trials.loc[df_trials.identifier == trial.identifier, column] = ', '.join(getattr(trial, column))
+            """
 
         if args.verbose >= 1:
             progress.update(1)
@@ -93,42 +96,32 @@ def trials_to_csv(args, trial_list, csv_path):
     :param csv_path:
     :return:
     """
-    df_trials = pd.DataFrame(columns=["HPE_done", "P2S_done", "OS_done", "MM_done", "stat_done",
-                                      "id_s", "id_p", "id_t", "identifier",
-                                      "affected", "measured_side", "cam_setting", "used_cams", "video_files",
-                                      "dir_session", "dir_participant", "dir_trial", "dir_calib", "dir_calib_videos", "json_list"])
+    df_trials = pd.DataFrame(columns=["HPE_done",
+                                      "calib_done",
+                                      "P2S_done",
+                                      "OS_done",
+                                      "MM_done",
+                                      "stat_done",
+                                      "id_s",
+                                      "id_p",
+                                      "id_t",
+                                      "identifier",
+                                      "affected",
+                                      "measured_side",
+                                      "cam_setting",
+                                      "used_cams",
+                                      "video_files",
+                                      "dir_session",
+                                      "dir_participant",
+                                      "dir_trial",
+                                      "dir_calib",
+                                      "dir_calib_videos",
+                                      "json_list"])
     for trial in trial_list:
         if args.verbose >= 2:
             print(f"adding trial {trial.identifier} to csv file.")
-        HPE_done = trial.HPE_done
-        calib_done = trial.calib_done
-        P2S_done = trial.P2S_done
-        OS_done = trial.OS_done
-        MM_done = trial.MM_done
-        stat_done = trial.stat_done
-        id_s = trial.id_s
-        id_p = trial.id_p
-        id_t = trial.id_t
-        identifier = trial.identifier
-        affected = trial.affected
-        measured_side = trial.measured_side
-        cam_setting = trial.cam_setting
-        used_cams = ", ".join(trial.used_cams)
-        video_files = ", ".join(trial.video_files)
-        dir_session = trial.dir_session
-        dir_participant = trial.dir_participant
-        dir_trial = trial.dir_trial
-        dir_calib = trial.dir_calib
-        dir_calib_videos = trial.dir_calib_videos
-        json_list = trial.json_list
-        new_row = pd.Series({"HPE_done": HPE_done, "calib_done": calib_done, "P2S_done": P2S_done, "OS_done": OS_done,
-                             "MM_done": MM_done, "stat_done": stat_done,
-                             "id_s": id_s, "id_p": id_p, "id_t": id_t,
-                             "identifier": identifier, "affected": affected, "measured_side": measured_side,
-                             "cam_setting": cam_setting, "used_cams": used_cams, "video_files": video_files,
-                             "dir_session": dir_session, "dir_participant": dir_participant,
-                             "dir_trial": dir_trial, "dir_calib": dir_calib, "dir_calib_videos": dir_calib_videos, "json_list": json_list})
-        df_trials = pd.concat([df_trials, new_row.to_frame().T], ignore_index=True)
+        df_trials = pd.concat([df_trials, get_new_row(trial, df_trials.columns).to_frame().T], axis=0, ignore_index=True)
+        pass
     df_trials.to_csv(csv_path, sep=';', index=False)
 
 
@@ -150,32 +143,32 @@ def trials_from_csv(args, df_trials, df_settings, root_data, default_dir):
     if args.verbose >= 1:
         progress = tqdm(total=df_trials.shape[0], desc="Creating Trial Objects", unit="Trial")
     for index, row in df_trials.iterrows():
-        id_s = row["setting_id"]
-        id_p = row["patient_id"]
-        id_t = row["trial_id"]
+        id_s = row["id_s"]
+        id_p = row["id_p"]
+        id_t = row["id_t"]
         identifier = row["identifier"]
-        s_dir = row["session_dir"]
-        p_dir = row["participant_dir"]
-        t_dir = row["trial_dir"]
-        videos = row["videos_used"].split(", ")
-        cams = row["cams_used"].split(", ")
+        dir_session = row["dir_session"]
+        dir_participant = row["dir_participant"]
+        dir_trial = row["dir_trial"]
+        video_files = row["video_files"].split(", ")
+        used_cams = row["used_cams"].split(", ")
         affected = row["affected"]
-        side = row["side"]
+        measured_side = row["measured_side"]
         dir_calib = row["dir_calib"]
         dir_calib_videos = row["dir_calib_videos"]
         pose_model = df_settings.loc[df_settings["setting_id"] == int(re.search("\d+", id_s).group()), "pose_model"].values[0]
         # Create the trial object
         trial = iDrinkTrial.Trial(identifier=identifier, id_s=id_s, id_p=id_p, id_t=id_t,
                                   dir_root=root_data, dir_default=default_dir,
-                                  dir_trial=t_dir, dir_participant=p_dir, dir_session=s_dir,
+                                  dir_trial=dir_trial, dir_participant=dir_participant, dir_session=dir_session,
                                   dir_calib=dir_calib, dir_calib_videos=dir_calib_videos,
-                                  affected=affected, measured_side=side,
-                                  video_files=videos, used_cams=cams,
+                                  affected=affected, measured_side=measured_side,
+                                  video_files=video_files, used_cams=used_cams,
                                   used_framework=args.poseback, pose_model=pose_model)
         trial.create_trial()
         trial.load_configuration()
-        trial.config_dict["pose"]["videos"] = videos
-        trial.config_dict["pose"]["cams"] = cams
+        trial.config_dict["pose"]["videos"] = trial.video_files
+        trial.config_dict["pose"]["cams"] = trial.used_cams
         trial.config_dict.get("project").update({"project_dir": trial.dir_trial})
         trial.config_dict['pose']['pose_framework'] = trial.used_framework
         trial.config_dict['pose']['pose_model'] = trial.pose_model
