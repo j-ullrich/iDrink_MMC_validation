a80463c63821c9cc1d9ee6b7db84a07a950e4d5c
j-ullrich <104524717+j-ullrich@users.noreply.github.com>
2024-09-09 16:23:31 +0200

    minor changes in Hub

---
+++ iDrinkCreateSettings.py
@@ -0,0 +1,10 @@
+"""This File is only used to create thee settings_csv for the iDrink Validation.
+
+The  Combinations should be "Randomised" within participants and trials.
+
+The used pose estimation Methods and used camera setups should be balanced but can have some overlap.
+
+OMC is done on all the data, therefore it is not influenced by the .csv file.
+"""
+
+# TODO: Implement
