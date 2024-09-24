c2ceb77c029545d72dfebd94614fb0e3d2203d35
j-ullrich <104524717+j-ullrich@users.noreply.github.com>
2024-09-24 17:27:09 +0200

    statistical analysis -- Resampling of time series if needed
    TODOList:
    - Check Biomechanical Models
    - Check Presets for Scaling and IK
    - Rework Murphy Measure Calculation
    - Add Statistical Analysis Script
    - Maybe Add Raw Keypoint Comparison --> Continuation of Marwens Thesis

--- src/iDrink/iDrinkStatisticalAnalysis.py
+++ src/iDrink/iDrinkStatisticalAnalysis.py
@@ -12,12 +12,43 @@ from fuzzywuzzy import process
 
 from trc import TRCData
 
+import math
 import pandas as pd
 import numpy as np
 import scipy as sp
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 from iDrinkOpenSim import read_opensim_file
+
+def resample_dataframes(df1, df2, max_time=None):
+    """
+    Resamples two DataFrames to the same amount of rows.
+
+    """
+    # TODO: Rework this function when Data of OMC and MMC of the same trial are available.
+    # Resample DataFrames to the same amount of frames
+    if max_time is None:
+        # Get the max time that is in both DataFrames. Last timestamp we can compare
+        max_time = math.ceil(min(df1['time'].iloc[-1], df2['time'].iloc[-1]))
+
+        # Cut DataFrames so that none exceeds the max_time, the shorter DF will be lower recording frequency
+        df1 = df1[df1['time'] <= max_time]
+        df2 = df2[df2['time'] <= max_time]
+
+    if df1['time'].iloc[-1] > df2['time'].iloc[-1]:
+        # make sure, df1 is always the shorter DataFrame when working on it. --> lower frequency
+        df2, df1 = resample_dataframes(df2, df1, max_time)
+
+    idx = []
+    for i in df1["time"]:
+        # Look for id with closest timestamp in df2 and add id to list.
+        idx.append(df2["time"].sub(i).abs().idxmin())
+
+    # Use Ids from list to create the new DataFrame
+    df2 = df2.iloc[list(map(int, idx))].reset_index(drop=True)
+
+    return df1, df2
+
 
 def runs_statistics_discrete(df_mmc, df_omc, root_stat):
     """
@@ -30,22 +61,50 @@ def runs_statistics_discrete(df_mmc, df_omc, root_stat):
     pass
 
 
-def run_statistics_continuous(df_mmc_pos, df_mmc_vel, df_omc_pos, df_omc_vel, isjoint, root_stat):
+def run_statistics_continuous(df_mmc, df_omc, to_compare, isjoint, root_stat, verbose=1):
     """
-    Gets two DataFrames form MMC and OMC. Once the Position of the Bodyparts and then their Velocity.
+    Compares two Dataframes containing positional data, velocity or acceleration data.
 
-    This function works with endeffector Pos/Vel and Joint Pos/Vel.
+    This function works with endeffector Pos/Vel/acc and Joint Pos/Vel/acc.
 
     It then plots the
 
-    :param df_mmc_pos:
+    :param df_mmc:
     :param df_mmc_vel:
-    :param df_omc_pos:
+    :param df_omc:
     :param df_omc_vel:
     :param isjoint:
     :param root_stat:
     :return:
     """
+
+    # Make sure DataFrames contain same amount of frames. (Rows)
+
+    if df_mmc.shape[0] != df_omc.shape[0]:
+        if verbose>=1:
+            print("DataFrames for Position do not contain the same amount of frames.\n"
+                  "Resampling DataFrames to the same amount of frames.")
+        df_mmc, df_omc = resample_dataframes(df_mmc, df_omc)
+
+
+    time_mmc = df_mmc['time']
+    time_omc = df_omc['time']
+
+    for column in to_compare:
+        if column in df_omc.columns:
+            pass
+        else:
+            raise ValueError(f"Data incompatible:\n"
+                             f"Column {column} not in df_omc_pos")
+        arr_mmc = df_mmc[column].to_numpy()
+        arr_omc = df_omc[column].to_numpy()
+
+
+
+
+
+
+
 
     pass
 
@@ -126,7 +185,18 @@ def standardize_data(df, metadata=None, verbose=1):
                        'radius_l_x','radius_l_y','radius_l_z','radius_l_ox','radius_l_oy','radius_l_oz',
                        'hand_l_x','hand_l_y','hand_l_z','hand_l_ox','hand_l_oy','hand_l_oz',
                        'center_of_mass_x','center_of_mass_y','center_of_mass_z']
-    stand_joints = []
+    stand_joints = ['time','pelvis_tilt','pelvis_list','pelvis_rotation','pelvis_tx','pelvis_ty','pelvis_tz',
+                    'hip_flexion_r','hip_adduction_r','hip_rotation_r',
+                    'knee_angle_r','knee_angle_r_beta','ankle_angle_r','subtalar_angle_r','mtp_angle_r',
+                    'hip_flexion_l','hip_adduction_l','hip_rotation_l',
+                    'knee_angle_l','knee_angle_l_beta','ankle_angle_l','subtalar_angle_l','mtp_angle_l',
+                    'L5_S1_Flex_Ext','L5_S1_Lat_Bending','L5_S1_axial_rotation','L4_L5_Flex_Ext','L4_L5_Lat_Bending',
+                    'L4_L5_axial_rotation','L3_L4_Flex_Ext','L3_L4_Lat_Bending','L3_L4_axial_rotation',
+                    'L2_L3_Flex_Ext','L2_L3_Lat_Bending','L2_L3_axial_rotation','L1_L2_Flex_Ext','L1_L2_Lat_Bending',
+                    'L1_L2_axial_rotation','L1_T12_Flex_Ext','L1_T12_Lat_Bending','L1_T12_axial_rotation',
+                    'Abs_r3','Abs_r2','Abs_r1','Abs_t1','Abs_t2','neck_flexion','neck_bending','neck_rotation',
+                    'arm_flex_r','arm_add_r','arm_rot_r','elbow_flex_r','pro_sup_r','wrist_flex_r','wrist_dev_r',
+                    'arm_flex_l','arm_add_l','arm_rot_l','elbow_flex_l','pro_sup_l','wrist_flex_l','wrist_dev_l']
 
 
     # List containing the standardized names of the bodypart values
@@ -160,11 +230,11 @@ def standardize_data(df, metadata=None, verbose=1):
     if 'elbow_flex_l' in df.columns:
 
         if verbose >=1:
-            print("Joint Data already standardized\n"
-                  "No further Standardization needed.")
+            print("Standardizing: \tJoint Data.")
 
         """Change columns to standardized names"""
-        df.columns = stand_joint
+        df.columns = standardize_columns(df.columns, stand_joints, verbose)
+
 
 
     elif any( i in df.columns for i in [' hand_l_x', 'hand_l_x', 'hand_l_X']):
@@ -179,7 +249,27 @@ def standardize_data(df, metadata=None, verbose=1):
 
     return kinematicState, df
 
-
+def get_dataframes(files):
+    for file in files:
+        metadata = None
+        if os.path.splitext(file)[1] == '.csv':
+            df_mmc_pos = pd.read_csv(file)
+            if 'pos' in file:
+                metadata = 'Coordinates'
+        else:
+            metadata, df = read_opensim_file(file)
+        kinematicState, df = standardize_data(df, metadata)
+        match kinematicState:
+            case 'pos':
+                df_pos = df
+            case 'vel':
+                df_vel = df
+            case 'acc':
+                df_acc = df
+            case _:
+                raise ValueError("Error in iDrinkStatisticalAnalysis\n"
+                                 "Kinematic State not recognized.")
+    return df_pos, df_vel, df_acc
 
 
 if __name__ == '__main__':
@@ -190,35 +280,87 @@ if __name__ == '__main__':
         print("Debug Mode is activated\n"
               "Starting debugging script.")
 
-    joint_kin_openism= {'pos': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_q.sto",
-                'vel': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_u.sto",
-                'acc': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_dudt.sto"}
-    body_kin_opensim = {'pos': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_pos_global.sto",
-                    'vel': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_vel_global.sto",
-                    'acc': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_acc_global.sto"}
+    """Set Root Paths for Processing"""
+    drives = ['C:', 'D:', 'E:', 'F:', 'G:' 'I:']
+    if os.name == 'posix':  # Running on Linux
+        drive = '/media/devteam-dart/Extreme SSD'
+    else:
+        drive = drives[1]
+
+    root_iDrink = os.path.join(drive, 'iDrink')
+    root_val = os.path.join(root_iDrink, "validation_root")
+    root_stat = os.path.join(root_val, '04_Statistics')
+
+    joints_of_interest = ['hip_flexion_r','hip_adduction_r','hip_rotation_r',
+                          'hip_flexion_l','hip_adduction_l','hip_rotation_l',
+                          'neck_flexion','neck_bending','neck_rotation',
+                          'arm_flex_r','arm_add_r','arm_rot_r',
+                          'elbow_flex_r','pro_sup_r',
+                          'wrist_flex_r','wrist_dev_r',
+                          'arm_flex_l','arm_add_l','arm_rot_l',
+                          'elbow_flex_l','pro_sup_l',
+                          'wrist_flex_l','wrist_dev_l'
+                          ]
+
+
+    """joint_kin_openism= {'pos': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_q.sto"),
+                'vel': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_u.sto"),
+                'acc': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_dudt.sto")}
+    body_kin_opensim = {'pos': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_pos_global.sto"),
+                    'vel': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_vel_global.sto"),
+                    'acc': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_acc_global.sto")}
 
     # Body Kin position by pose2sim
-    body_kin_p2s = {'pos': r"I:\iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_p2s\S003_P07_T043_affected_Body_kin_p2s_pos.csv"
-                }
+    body_kin_p2s = {'pos': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_p2s\S003_P07_T043_affected_Body_kin_p2s_pos.csv")
+                }"""
+
+    joint_kin_openism = {
+        'pos': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_q.sto"),
+        'vel': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_u.sto"),
+        'acc': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_Kinematics_dudt.sto")}
+    body_kin_opensim = {
+        'pos': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_pos_global.sto"),
+        'vel': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_vel_global.sto"),
+        'acc': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_opensim_analyzetool\S003_P07_T043_BodyKinematics_acc_global.sto")}
+
+    joint_kin_OMC = {
+        'pos': os.path.join(drive, r"iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_opensim_analyzetool\S15133_P01_T001_Kinematics_q.sto"),
+        'vel': os.path.join(drive, r"iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_opensim_analyzetool\S15133_P01_T001_Kinematics_u.sto"),
+        'acc': os.path.join(drive, r"iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_opensim_analyzetool\S15133_P01_T001_Kinematics_dudt.sto")}
+    body_kin_OMC = {
+        'pos': os.path.join(drive, r"iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_opensim_analyzetool\S15133_P01_T001_BodyKinematics_pos_global.sto"),
+        'vel': os.path.join(drive, r"iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_opensim_analyzetool\S15133_P01_T001_BodyKinematics_vel_global.sto"),
+        'acc': os.path.join(drive, r"iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_opensim_analyzetool\S15133_P01_T001_BodyKinematics_acc_global.sto")}
+
+    body_kin_p2s_OMC = {
+        'pos': os.path.join(drive, r"D:\iDrink\validation_root\03_data\OMC\S15133\S15133_P01\S15133_P01_T001\movement_analysis\kin_p2s\S15133_P01_T001_Body_kin_p2s_pos.csv")
+    }
+
+    # Body Kin position by pose2sim
+    body_kin_p2s = {
+        'pos': os.path.join(drive, r"iDrink\validation_root\03_data\setting_003\P07\S003\S003_P07\S003_P07_T043\movement_analysis\kin_p2s\S003_P07_T043_affected_Body_kin_p2s_pos.csv")
+        }
 
     files = {'joint_kin_openism' : joint_kin_openism,
              'body_kin_opensim' : body_kin_opensim,
              'body_kin_p2s' : body_kin_p2s}
 
-    file = files.get('joint_kin_openism').get('pos')
-
+    files_mmc = [joint_kin_openism.get('pos'), joint_kin_openism.get('vel'), joint_kin_openism.get('acc')]
     continuous = True
+    isjoint = True
 
 
-    metadata = None
-    if os.path.splitext(file)[1] == '.csv':
-        df_mmc_pos = pd.read_csv(file)
-        if 'pos' in file:
-            metadata = 'Coordinates'
-    else:
-        metadata, df_mmc_pos = read_opensim_file(file)
+    df_mmc_pos, df_mmc_vel, df_mmc_acc = get_dataframes(files_mmc)
 
-    kinematicState, df_mmc_pos = standardize_data(df_mmc_pos, metadata)
+    files = {
+        'joint_kin_OMC' : joint_kin_OMC,
+        'body_kin_OMC' : body_kin_OMC,
+        'body_kin_p2s_OMC' : body_kin_p2s_OMC
+    }
+
+    files_omc = [joint_kin_OMC.get('pos'), joint_kin_OMC.get('vel'), joint_kin_OMC.get('acc')]
+
+    df_omc_pos, df_omc_vel, df_omc_acc = get_dataframes(files_omc)
 
     if continuous:
-        run_statistics_continuous(df_mmc_pos, df_mmc_vel, df_omc_pos, df_omc_vel, isjoint, root_stat)
+        run_statistics_continuous(df_mmc_pos, df_omc_pos, joints_of_interest, isjoint, root_stat)
